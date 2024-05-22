e9699acddc055c5f94d19823b1748576bf1a7798
Stefan Licklederer <Stefan.Licklederer@tum.de>
2024-05-22 23:09:37 +0200

    Fixed remaining "problems". Renderer2.h complains about a problem in Drawables.h but Drawables.h shows no problems... Who knows

--- Classes.cpp
+++ Classes.cpp
@@ -3,17 +3,17 @@
 
 
 
-int main(int argc, char** argv) {
+int main() {
     //typedefs
     typedef std::array<double, 3> arr3d;
-    typedef std::array<double, 4> arr4d;
-    typedef std::vector<Photon> vPhoton;
+
     // initiate random seed
     std::srand(time(NULL));
+    
     // Initialize Emission Setup
-    // Initiate BlackHole
+      // Initiate BlackHole
     BlackHole BH = BlackHole(0.98);
-    // Set up position and Velocity
+      // Set up position and Velocity
     CoordVec3 bl_source_position_cart = CoordVec3(2.5, 0.0, 0.001);
     CoordVec3 bl_source_position = bl_source_position_cart.to_bl(BH.get_a_spin());
     //CoordVec3 bl_source_position2 = CoordVec3(0.0, 0.0, 4.5).to_bl(BH.get_a_spin());
@@ -22,7 +22,7 @@ int main(int argc, char** argv) {
     arr3d source_position = {bl_source_position.x, std::sin(bl_source_position.y), std::cos(bl_source_position.y)};
     arr3d source_velocity = {0.0, 0.0, 0.0}; // x,y,z velocities
     //arr3d source_position2 = {bl_source_position2.x, std::sin(bl_source_position2.y), std::cos(bl_source_position2.y)};
-      //
+    //
     Emission_Setup ESt = Emission_Setup(source_position, source_velocity, 200, BH.get_a_spin());
     //Emission_Setup ESt2 = Emission_Setup(source_position2, source_velocity, 100, BH.get_a_spin());
 
@@ -44,12 +44,12 @@ int main(int argc, char** argv) {
     DrawableBLSphere Draw_BH = DrawableBLSphere(BH.get_rHp(), BH.get_a_spin());
     std::vector<DrawableLine> PhotonDraws = {};
     //std::vector<DrawableLine> PhotonDraws2 = {};
-    for(int j = 0; j < ESt.get_EmittedPhotons().size(); j++)
+    for(int j = 0; j < int(ESt.get_EmittedPhotons().size()); j++)
     {
         PhotonDraws.push_back(ESt.get_EmittedPhotons()[j].get_Drawable());
         //PhotonDraws2.push_back(ESt2.get_EmittedPhotons()[j].get_Drawable());
     }
-    for(int j = 0; j < PhotonDraws.size(); j++){
+    for(int j = 0; j < int(PhotonDraws.size()); j++){
         renderer.addDrawable(&PhotonDraws[j]);
         //renderer.addDrawable(&PhotonDraws2[j]);
     }
